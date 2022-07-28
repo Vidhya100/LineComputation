e8ae26fced2c9522cab01e2b01041feacd8b7019
Vidhya100 <vidhyadarade1@gmail.com>
2022-07-28 17:54:05 +0530

    Equality of lines checked

--- Program.cs
+++ Program.cs
@@ -13,7 +13,13 @@
             double x2 = Convert.ToDouble(Console.ReadLine());
             double y2 = Convert.ToDouble(Console.ReadLine());
 
-            LineComputation.LengthOfLine(x1, y1, x2, y2);
+            Console.WriteLine("Enter the line co-ordinates as a1,b1,a2,b2: ");
+            double a1 = Convert.ToDouble(Console.ReadLine());
+            double b1 = Convert.ToDouble(Console.ReadLine());
+            double a2 = Convert.ToDouble(Console.ReadLine());
+            double b2 = Convert.ToDouble(Console.ReadLine());
+
+            LineComputation.LengthOfLine(x1, y1, x2, y2, a1, b1, a2, b2);
         }
     }
 }
