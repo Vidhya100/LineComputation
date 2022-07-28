c5776e03a03058dcf1b4d80961f57d40aac9ccfb
Vidhya100 <vidhyadarade1@gmail.com>
2022-07-28 17:28:36 +0530

    Greating message added

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace LineComparison
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+                        
+            Console.WriteLine("Welcome to the  Line Comparion Program.");
+
+            Console.WriteLine("Enter the line co-ordinates as x1,y1,x2,y2: ");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            double y2 = Convert.ToDouble(Console.ReadLine());
+
+            LineComputation.LengthOfLine(x1, y1, x2, y2);
+        }
+    }
+}
