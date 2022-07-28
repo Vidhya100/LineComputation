45dd7ab4ff4373e72f518f456f9b3af63a103af8
Vidhya100 <vidhyadarade1@gmail.com>
2022-07-28 17:35:40 +0530

    Length of line calculated

---
+++ LineComputation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    /// <summary>
+    /// Finding length of line using cartesian co-ordinates
+    /// </summary>
+    public static class LineComputation
+    {
+
+        /// <summary>
+        /// Lengthes the of line.
+        /// </summary>
+        /// <param name="x1">The x1.</param>
+        /// <param name="y1">The y1.</param>
+        /// <param name="x2">The x2.</param>
+        /// <param name="y2">The y2.</param>
+        public static void LengthOfLine(double x1, double y1, double x2, double y2)
+        {
+            double lengthOfLine;
+            lengthOfLine = Math.Sqrt( Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) );
+
+            Console.WriteLine("Length of a given Line is: " + lengthOfLine);
+
+        }
+    }
+}
